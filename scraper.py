8fee0ed8d482c15c7a775ab92ae0d0ad7214dfcf
chentihe <chentihe@gmail.com>
2021-02-24 12:23:39 +0800

    pttalltogether v2

--- scraper.py
+++ scraper.py
@@ -2,30 +2,40 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-def scraper(token):
+def scraper():
     rs = requests.session()
-    response = rs.get('https://www.ptt.cc/bbs/Alltogether/index.html')
-    soup = BeautifulSoup(response.text, 'html.parser')
+    url = 'https://www.ptt.cc/bbs/Alltogether/index.html'
+
     local_time = time.localtime()
     timeString = time.strftime('%m/%d', local_time) #顯示 月/日
     if timeString[0] == '0':
         timeString = timeString[1:] # 讓 月/日格式和ptt一樣
-    for entry in soup.select(' .r-ent'):
-        if ('徵男' in entry.select(' .title')[0].text) and (entry.select('.date')[0].text.strip() == timeString):
-            headers = {
-                'Authorization': 'Bearer ' + token,
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-            params = {'message': entry.select(' .title')[0].text + '\n' +
-            'https://www.ptt.cc' + entry.find('a').get('href')}
 
-            r = requests.post('https://notify-api.line.me/api/notify',
-                                headers=headers, params=params)
-            
-            return r.status_code
+    for _ in range(5):
+        response = rs.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        previousPage = soup.select('div.btn-group.btn-group-paging a')[1]['href']
+        url = 'https://www.ptt.cc' + previousPage
+
+        for target in soup.select('.r-ent'):
+            if ('徵男' in target.select(' .title')[0].text) and (target.select('.date')[0].text.strip() == timeString):
+                
+                title = target.find(class_='title').text.strip()
+                link = 'https://www.ptt.cc' + target.find('a').get('href')
+
+                headers = {
+                    'Authorization': 'Bearer ' + token,
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+                params = {'message': title + '\n' +
+                'https://www.ptt.cc' + link}
+                r = requests.post('https://notify-api.line.me/api/notify',
+                                    headers=headers, params=params)
+                return r.status_code
         time.sleep(10)
+
 
 if __name__ == '__main__':
     token = 'D7fZ04VxabHBFobJN7IxDN9BwNOc2HkboyumpcFMGDm'
 
-    scraper(token)
+scraper()
